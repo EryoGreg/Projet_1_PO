209412eb3c9285f0da9abbf4a64d94613fc33bd0
Unknown <elouan.dz@gmail.com>
2018-01-09 16:07:08 +0100

    CollectionCatégorie complete

---
+++ src/bean_Produit/Produit.java
@@ -0,0 +1,5 @@
+package bean_Produit;
+
+public class Produit {
+
+}
--- src/bean_catégorie/CollectionCategorie.java
+++ src/bean_catégorie/CollectionCategorie.java
@@ -1,5 +1,61 @@
-package bean_catégorie;
+package bean_Catégorie;
 
-public class CollectionCategorie {
+import java.util.ArrayList;
+import bean_Produit.*;
+
+@SuppressWarnings("serial")
+public class CollectionCategorie extends ArrayList<Categorie>{
+	
+	private int curseur = 0;
+
+	public CollectionCategorie(int curseur) {
+		this.curseur = curseur;
+	}
+	
+	public Categorie getCatégorieCourante() {
+		return this.get(curseur);
+	}
+	
+	public void suivant() {
+		// TODO Auto-generated method stub
+		if (curseur == this.size()-1) {
+			return;
+		}
+		curseur++;
+
+	}
+	
+	public void precedent() {
+		// TODO Auto-generated method stub
+		if (curseur == 0) {
+			return;
+		}
+		curseur--;
+
+	}
+	
+	public void premier() {
+		// TODO Auto-generated method stub
+		curseur = 0;
+
+	}
+	public void dernier() {
+		// TODO Auto-generated method stub
+		curseur=this.size()-1;
+
+	}
+	public boolean estPremier() {
+		// TODO Auto-generated method stub
+		if (curseur == 0) {
+			return true;
+		}
+		return false;
+	}
+	public boolean estDernier() {
+	if (curseur == this.size()-1) {
+		return true;
+	}
+	return false;
+	}
 
 }
